4fdd54dc20b05456710879b507f4318fee03beb2
KandyKannan <125741068+KandyKannan@users.noreply.github.com>
2025-02-07 23:48:27 +0530

    Add files via upload

---
+++ valentine.js
@@ -0,0 +1,26 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.noButton');
+    const yesButton = document.querySelector('.yesButton');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+function handleYesClick() {
+    window.location.href = "YesPage.html";
+}
